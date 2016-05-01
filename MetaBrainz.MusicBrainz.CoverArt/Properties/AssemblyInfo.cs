a231d217580014400e736947c60098723891c3e2
Tim Van Holder <tim.vanholder@gmail.com>
2016-05-01 19:33:12 +0200

    Added a libcoverart port, MetaBrainz.MusicBrainz.CoverArt.

---
+++ MetaBrainz.MusicBrainz.CoverArt/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle        ("MetaBrainz.MusicBrainz.CoverArt")]
+[assembly: AssemblyDescription  (".NET Implementation of libcoverart")]
+[assembly: AssemblyCompany      ("MetaBrainz")]
+[assembly: AssemblyProduct      ("libcoverart")]
+[assembly: AssemblyCopyright    ("Copyright © Tim Van Holder 2016")]
+
+[assembly: ComVisible(false)]
+
+// This should be MAJOR.MINOR, matching libcoverart.
+[assembly: AssemblyVersion    ("1.0")]
+
+// This should be MAJOR.MINOR.PATCHLEVEL, with MAJOR.MINOR matching libcoverart and PATCHLEVEL as a release sequence number (not following libcoverart).
+[assembly: AssemblyFileVersion("1.0.0")]
+
+// This should match AssemblyFileVersion, plus optional extra tags (such as a pre-release indicator).
+[assembly: AssemblyInformationalVersion("1.0.0 (pre-release)")]
