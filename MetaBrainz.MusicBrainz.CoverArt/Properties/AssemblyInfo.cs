bf5a9ccfe11392f59dfe11be2574a4eab96bc6f8
Tim Van Holder <tim.vanholder@gmail.com>
2016-06-24 19:31:53 +0200

    Reset all versions to 0.1. Updated the README. I'll look at matching libxxx versions when these libraries are more stable API-wise.

--- MetaBrainz.MusicBrainz.CoverArt/Properties/AssemblyInfo.cs
+++ MetaBrainz.MusicBrainz.CoverArt/Properties/AssemblyInfo.cs
@@ -9,11 +9,8 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-// This should be MAJOR.MINOR, matching libcoverart.
-[assembly: AssemblyVersion    ("1.0")]
-
-// This should be MAJOR.MINOR.PATCHLEVEL, with MAJOR.MINOR matching libcoverart and PATCHLEVEL as a release sequence number (not following libcoverart).
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion    ("0.1")]
+[assembly: AssemblyFileVersion("0.1.0")]
 
 // This should match AssemblyFileVersion, plus optional extra tags (such as a pre-release indicator).
-[assembly: AssemblyInformationalVersion("1.0.0 (pre-release)")]
+[assembly: AssemblyInformationalVersion("0.1.0 (pre-release)")]
