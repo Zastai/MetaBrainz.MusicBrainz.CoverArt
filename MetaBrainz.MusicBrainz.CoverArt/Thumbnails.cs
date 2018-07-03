4ea4c8b644106a7b8236446a7099e87efa6adfa5
Tim Van Holder <tim.vanholder@gmail.com>
2018-07-03 22:53:31 +0200

    CAA-88: Adjust thumbnail types. Dropped Huge (huge); added Size250 (250), Size500 (500) and Size1200 (1200).

--- MetaBrainz.MusicBrainz.CoverArt/Thumbnails.cs
+++ MetaBrainz.MusicBrainz.CoverArt/Thumbnails.cs
@@ -14,17 +14,25 @@ namespace MetaBrainz.MusicBrainz.CoverArt {
   [JsonObject(MemberSerialization.OptIn)]
   public sealed class Thumbnails {
 
-    /// <summary>The URI for the small (250px) thumbnail of the image, if available.</summary>
+    /// <summary>The URI for the small thumbnail of the image, if available.</summary>
     [JsonProperty("small")]
     public Uri Small { get; private set; }
 
-    /// <summary>The URI for the large (500px) thumbnail of the image, if available.</summary>
+    /// <summary>The URI for the large thumbnail of the image, if available.</summary>
     [JsonProperty("large")]
     public Uri Large { get; private set; }
 
-    /// <summary>The URI for the huge (1200px) "thumbnail" of the image, if available.</summary>
-    [JsonProperty("huge")]
-    public Uri Huge { get; private set; }
+    /// <summary>The URI for the 250-pixel thumbnail of the image, if available.</summary>
+    [JsonProperty("250")]
+    public Uri Size250 { get; private set; }
+
+    /// <summary>The URI for the 500-pixel thumbnail of the image, if available.</summary>
+    [JsonProperty("500")]
+    public Uri Size500 { get; private set; }
+
+    /// <summary>The URI for the 1200-pixel "thumbnail" of the image, if available.</summary>
+    [JsonProperty("1200")]
+    public Uri Size1200 { get; private set; }
 
   }
 
